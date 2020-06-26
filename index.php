d3e5adf4d6c6f74509833ebc65f917e5f4517b05
RafaelZamora <rafael_zamora99@hotmail.com>
2020-06-25 22:58:27 -0500

    Obtener datos desde respuesta

--- index.php
+++ index.php
@@ -21,29 +21,30 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </html>
 <?php
+    //Datos de acceso
     $User = $_POST['User'];
     $Password = $_POST['Password'];
     //Realizar conexión al cliente
     $soapClient = new SoapClient('https://demo.linntae.mx/services/ventasSoap?wsdl');
     //Crear la lista de parametros
     $params = array('user' => $User, 'password' => $Password);
-    $paramsTae = array('user' => 'demoLinntaews', 
+    /*$paramsTae = array('user' => 'demoLinntaews', 
                 'password' => '1234', 
                 'carrier' => '1', 
                 'telefono' => '2295242552', 
-                'monto' => '20');
+                'monto' => '20');*/
     //Crear la petición de consulta saldo
     $consultaSaldo = $soapClient->saldo($params);
     var_dump($consultaSaldo);
-    $recargaTae = $soapClient->recargaTae($paramsTae);
+    echo $consultaSaldo->data;
+    //$recargaTae = $soapClient->recargaTae($paramsTae);
     //Procesar la respuesta
+    $json = json_encode($consultaSaldo);
     $jsonConsultaSaldo = json_decode(json_encode($consultaSaldo), true);  
-    $jsonRecargaTae = json_decode(json_encode($recargaTae),true);
-
-    print_r($jsonConsultaSaldo);
+    //$jsonRecargaTae = json_decode(json_encode($recargaTae),true);
+    $doc = new DOMDocument;
+    $doc->loadXML($consultaSaldo->data);
     echo "<br>";
-    print_r($jsonRecargaTae);
-    foreach ($jsonConsultaSaldo as $item) {
-        echo $item;
-    }
+    echo 'Saldo restante: $' . $doc->getElementsByTagName('saldoPlataforma')->item(0)->nodeValue . '<br>';
+    echo 'Comisión: $' . $doc->getElementsByTagName('saldoComision')->item(0)->nodeValue;
 ?>
