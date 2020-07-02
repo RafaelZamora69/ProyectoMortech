90e7125190ce5047a1bb7033c0d8ecbf85b5ee13
RafaelZamora <rafael_zamora99@hotmail.com>
2020-07-01 19:45:40 -0500

    Bd

--- Database/mortechsaldo.sql
+++ Database/mortechsaldo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 29-06-2020 a las 23:28:18
+-- Tiempo de generación: 02-07-2020 a las 00:43:25
 -- Versión del servidor: 10.4.10-MariaDB
 -- Versión de PHP: 7.3.12
 
@@ -21,6 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `mortechsaldo`
 --
+CREATE DATABASE IF NOT EXISTS `mortechsaldo` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `mortechsaldo`;
 
 -- --------------------------------------------------------
 
@@ -33,14 +35,15 @@ CREATE TABLE IF NOT EXISTS `cliente` (
   `idCliente` int(11) NOT NULL AUTO_INCREMENT,
   `Nombre` varchar(70) NOT NULL,
   PRIMARY KEY (`idCliente`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `cliente`
 --
 
 INSERT INTO `cliente` (`idCliente`, `Nombre`) VALUES
-(1, 'Cliente de prueba');
+(1, 'Cliente de prueba'),
+(2, 'caca');
 
 -- --------------------------------------------------------
 
@@ -87,18 +90,29 @@ CREATE TABLE IF NOT EXISTS `venta` (
   `Pagado` tinyint(1) NOT NULL,
   `Observaciones` varchar(300) NOT NULL,
   `Fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `Corte` tinyint(1) DEFAULT 0,
   PRIMARY KEY (`idVenta`),
   KEY `idCliente` (`idCliente`),
   KEY `idVendedor` (`idEmpleado`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `venta`
 --
 
-INSERT INTO `venta` (`idVenta`, `idCliente`, `idEmpleado`, `NombreServicio`, `NumeroTelefono`, `Operadora`, `Monto`, `PrecioVenta`, `Pagado`, `Observaciones`, `Fecha`) VALUES
-(1, 1, 14, 'Recarga Saldo', '2292235413', 'Telcel', 20, 50, 1, 'Observación de prueba', '2020-06-29 18:58:31'),
-(2, 1, 14, 'Recarga Saldo', '2292235413', 'Telcel', 20, 50, 1, 'Observación de prueba', '2020-06-29 18:58:43');
+INSERT INTO `venta` (`idVenta`, `idCliente`, `idEmpleado`, `NombreServicio`, `NumeroTelefono`, `Operadora`, `Monto`, `PrecioVenta`, `Pagado`, `Observaciones`, `Fecha`, `Corte`) VALUES
+(1, 1, 14, 'Recarga Saldo', '2292235413', 'Telcel', 20, 50, 1, 'Observación de prueba', '2020-06-29 18:58:31', 0),
+(2, 1, 14, 'Recarga Saldo', '2292235413', 'Telcel', 20, 50, 1, 'Observación de prueba', '2020-06-29 18:58:43', 0),
+(3, 2, 14, 'Recarga de salgo', '22', 'Telcel', 23, 23, 0, 'c', '2020-07-01 15:17:26', 0),
+(4, 2, 14, 'Recarga de salgo', '2292141516', 'Telcel', 23, 23, 0, 'InserciÃ³n de varios nÃºmeros', '2020-07-01 15:18:55', 0),
+(5, 2, 14, 'Recarga de salgo', '22441225', 'Telcel', 23, 23, 0, 'InserciÃ³n de varios nÃºmeros', '2020-07-01 15:18:55', 0),
+(6, 2, 14, 'Recarga de salgo', '22995447', 'Telcel', 23, 23, 0, 'InserciÃ³n de varios nÃºmeros', '2020-07-01 15:18:55', 0),
+(7, 2, 14, 'Recarga de salgo', '2292141516', 'Telcel', 20, 40, 0, 'Prueba con la API', '2020-07-01 16:13:35', 0),
+(8, 2, 14, 'Recarga de salgo', '2292141516', 'Telcel', 20, 40, 0, 'Prueba con la API', '2020-07-01 19:39:37', 0),
+(9, 2, 14, 'Recarga de salgo', '2292141516', 'Telcel', 20, 40, 0, 'Prueba con la API', '2020-07-01 19:40:01', 0),
+(10, 2, 14, 'Recarga de salgo', '2292141687', 'Telcel', 20, 20, 1, 'Prueba 2', '2020-07-01 19:47:38', 0),
+(11, 2, 14, 'Recarga de salgo', '2299345267', 'Telcel', 20, 20, 1, 'Prueba 2', '2020-07-01 19:48:11', 0),
+(12, 2, 14, 'Recarga de salgo', '2292834353', 'Telcel', 20, 20, 1, 'Prueba 2', '2020-07-01 19:48:13', 0);
 
 --
 -- Restricciones para tablas volcadas
