0291830ce06cf0480df9703882d8b28a484932f5
RafaelZamora <rafael_zamora99@hotmail.com>
2020-07-08 15:03:09 -0500

    Control de acceso

--- views/servicio.php
+++ views/servicio.php
@@ -1,3 +1,8 @@
+<?php 
+    if($_SESSION['identity'] == null){
+        header('Location: ' . base_url . 'login/index');
+    } 
+?>
 <div class="centerDiv">
 <div class="row">
     <div class="col s12">
