056c676af442b530a2ed7aff837da9ec97646272
RafaelZamora <rafael_zamora99@hotmail.com>
2020-07-09 11:29:03 -0500

    Login

--- config/conexion.php
+++ config/conexion.php
@@ -1,7 +1,7 @@
 <?php
     class dbConnect {
         public function connect() {
-            $connection = mysqli_connect('localhost', 'root', '', 'mortechsaldo');
+            $connection = mysqli_connect('localhost', 'mortech_login', 'M0rT3cHx099$%&', 'mortech_saldo');
             $connection->query("set names 'utf-8'");
             return $connection;
         }
--- config/parameters.php
+++ config/parameters.php
@@ -1,4 +1,4 @@
 <?php
-    define("base_url", "http://".$_SERVER['HTTP_HOST']."/proyectoMortech/");
+    define("base_url", "http://".$_SERVER['HTTP_HOST']."/ProyectoMortech/");
     define("defaultController", "loginController");
     define("defaultAction", "index");
