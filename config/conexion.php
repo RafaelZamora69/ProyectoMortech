08f431e5bc7ef20801299cadd6ec89958688f694
RafaelZamora <rafael_zamora99@hotmail.com>
2020-07-18 11:31:52 -0500

    Cambios en conexión

--- config/conexion.php
+++ config/conexion.php
@@ -1,18 +1,22 @@
 <?php
-    class dbConnect {
-        public function connect() {
-            $connection = mysqli_connect('localhost', 'root', '', 'mortechsaldo');
-            $connection->query("set names 'utf-8'");
-            return $connection;
-        }
-
-        public function LoginVendedor(){
-            $connection = mysqli_connect('localhost', 'mortech_vendedor', 'M0rT3cHx099$%&', 'mortech_saldo');
-            return $connection;
-        }
-
-        public function LoginAdmin(){
-            $connection = mysqli_connect('localhost', 'mortech_admin', 'M0rT3cHx099$%&', 'mortech_saldo');
-            return $connection;
-        }
+class dbConnect
+{
+    public function connect()
+    {
+        $connection = mysqli_connect('localhost', 'root', '', 'mortech_saldo');
+        $connection->query("set names 'utf-8'");
+        return $connection;
     }
+
+    public function LoginVendedor()
+    {
+        $connection = mysqli_connect('localhost', 'mortech_vendedor', 'M0rT3cHx099$%&', 'mortech_saldo');
+        return $connection;
+    }
+
+    public function LoginAdmin()
+    {
+        $connection = mysqli_connect('localhost', 'mortech_admin', 'M0rT3cHx099$%&', 'mortech_saldo');
+        return $connection;
+    }
+}
