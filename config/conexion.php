005fa915eabb5b9e4c884d709cea7a876dae78e2
RafaelZamora <rafael_zamora99@hotmail.com>
2020-07-20 12:10:37 -0500

    :angry:

--- config/conexion.php
+++ config/conexion.php
@@ -1,10 +1,12 @@
 <?php
-    class dbConnect {
-        public function connect() {
-            $connection = mysqli_connect('localhost', 'mortech_login', 'M0rT3cHx099$%&', 'mortech_saldo');
-            $connection->query("set names 'utf-8'");
-            return $connection;
-        }
+class dbConnect
+{
+    public function connect()
+    {
+        $connection = mysqli_connect('localhost', 'mortech_login', 'M0rT3cHx099$%&', 'united_saldo');
+        $connection->query("set names 'utf-8'");
+        return $connection;
+    }
 
     public function LoginVendedor()
     {
