11066e1ff098149f724882bf7d0542e3e682d156
RafaelZamora <rafael_zamora99@hotmail.com>
2020-08-04 17:09:16 -0500

    Cosas

--- js/reportes.js
+++ js/reportes.js
@@ -6,6 +6,8 @@ document.addEventListener('DOMContentLoaded', function () {
     var clientes = M.Autocomplete.init(elems);
     var elems = document.getElementById('autoCompleteEmpleados');
     var empleados = M.Autocomplete.init(elems);
+    var elems = document.getElementById('NombreCliente');
+    var clientesModal = M.Autocomplete.init(elems);
     var elems = document.querySelectorAll('.datepicker');
     var today = new Date();
     var fechas = M.Datepicker.init(elems, {
@@ -112,7 +114,7 @@ document.addEventListener('DOMContentLoaded', function () {
             .then(res => res.json())
             .then(res => {
                 document.getElementById("NombreEmpleado").textContent = res[0].Empleado;
-                document.getElementById("NombreCliente").textContent = res[0].Cliente;
+                document.getElementById("NombreCliente").value = res[0].Cliente;
                 document.getElementById("Mxn").value = res[0].Mxn;
                 document.getElementById("Usd").value = res[0].Usd;
                 document.getElementById("Observaciones").value = res[0].Observaciones;
@@ -168,7 +170,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function TablaSaldo(data) {
         CargarTablaSaldo();
-        for (var i = 1; i < data.length; i++) {
+        for (let i = 1; i < data.length; i++) {
             var tr = document.createElement("tr");
             var td = document.createElement("td");
             td.innerText = data[i].Empleado;
@@ -196,7 +198,7 @@ document.addEventListener('DOMContentLoaded', function () {
             tr.appendChild(td);
             var editar = document.createElement("a");
             editar.textContent = 'Editar';
-            editar.classList.add('waves-effect', 'waves-light', 'yellow', 'btn');
+            editar.classList.add('waves-effect', 'waves-light', 'yellow', 'btn', 'black-text');
             editar.onclick = function () {
                 obtenerDatos(data[i].idVenta);
             }
@@ -344,6 +346,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     data[res[i]] = null;
                 }
                 clientes.updateData(data);
+                clientesModal.updateData(data);
             })
     }
 
@@ -364,6 +367,7 @@ document.addEventListener('DOMContentLoaded', function () {
         let array = Array.from(document.getElementsByName("Servicio"));
         for (i in array) {
             if (array[i].checked == true) {
+                console.log(array[i].id);
                 Servicio = array[i].id;
             }
         }
