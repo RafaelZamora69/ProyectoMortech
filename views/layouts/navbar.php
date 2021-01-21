0fb1843817bef04673f385970c1863d18406dec2
Rafael Zamora <rafael_zamora99@hotmail.com>
2021-01-20 19:00:37 -0600

    Mejoras wip

--- views/layouts/navbar.php
+++ views/layouts/navbar.php
@@ -17,33 +17,8 @@ $api->setPassword($_SESSION['identity']['Password']);
 <body>
     <nav class="red">
         <div class="nav-wrapper">
-            <a href="#" data-target="menu" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+            <a href="#" data-target="menu" class="sidenav-trigger show-on-large"><i class="material-icons">menu</i></a>
             <a href="#">Saldo: $<?= $api->saldoPlataforma() ?></a>
-            <a href="#"><?= $_SESSION['identity']['Nombre'] ?></a>
-            <ul class="right hide-on-small-only">
-                <li></li>
-                <li><a href="#" class="dropdown-trigger" data-target="dropdownMenu">Menu<i class="material-icons right">arrow_drop_down</i></a></li>
-            </ul>
         </div>
     </nav>
-    <!-- dropDown -->
-    <ul id="dropdownMenu" class="dropdown-content">
-        <li><a class="waves-effect" href="<?= base_url ?>principal/index">Inicio</a></li>
-        <li class="divider"></li>
-        <li><a class="waves-effect" href="<?= base_url ?>servicios/index">Venta de servicio</a></li>
-        <li><a href="<?= base_url ?>cliente/index" class="waves-effect">Busqueda de cliente</a></li>
-        <li><a href="<?= base_url ?>compra/usuario" class="waves-effect">Mis compras</a></li>
-        <li><a href="<?= base_url ?>servicios/usuarios" class="waves-effect">Modificaciones</a></li>
-        <?php if ($_SESSION['identity']['Jerarquia'] == 'Administrador') : ?>
-            <li class="divider"></li>
-            <li><a href="<?= base_url ?>compra/index" class="waves-effect">Compras</a></li>
-            <li><a href="<?= base_url ?>verificar/index" class="waves-effect">Verificar servicios</a></li>
-            <li><a href="<?= base_url ?>reportes/index" class="waves-effect">Reportes</a></li>
-            <li><a href="<?= base_url ?>corte/index" class="waves-effect">Corte</a></li>
-            <li><a href="<?= base_url ?>utilidad/index" class="waves-effect">Utilidades</a></li>
-            <li><a href="<?= base_url ?>usuario/index" class="waves-effect">Usuarios</a></li>
-        <?php endif ?>
-        <li class="divider"></li>
-        <li><a href="<?= base_url ?>login/logout" class="waves-effect">Cerrar sesión</a></li>
-    </ul>
     <div class="container">
--- views/layouts/sidenav.php
+++ views/layouts/sidenav.php
@@ -1,35 +1,75 @@
 <!-- sideNav -->
-<ul class="sidenav" id="menu">
-    <li>
-        <div class="user-view">
-            <div class="background">
-                <img src="../images/background.png" alt="background image" class="responsive-img">
+<div class="">
+    <ul class="sidenav sidenav-fixed" id="menu">
+        <li>
+            <div class="user-view">
+                <div class="background">
+                    <img src="../images/background.png" alt="background image" class="responsive-img">
+                </div>
+                <a href="#user"><img src="../images/userProfile.jpg" alt="userProfile" class="circle"></a>
+                <a href="#name"><span class="white-text name" id="Name"><?= $_SESSION['identity']['Nombre'] ?></span></a>
+                <a href="#mail"><span class="white-text email">example@domain.com</span></a>
             </div>
-            <a href="#user"><img src="../images/userProfile.jpg" alt="userProfile" class="circle"></a>
-            <a href="#name"><span class="white-text name" id="Name"><?= $_SESSION['identity']['Nombre'] ?></span></a>
-            <a href="#mail"><span class="white-text email">example@domain.com</span></a>
-        </div>
-    </li>
-    <li><a class="waves-effect" href="<?= base_url ?>principal/index">Inicio</a></li>
-    <li>
-        <div class="divider"></div>
-    </li>
-    <li><a class="subheader">Operaciones</a></li>
-    <li><a href="<?= base_url ?>servicios/index" class="waves-effect">Venta de servicio</a></li>
-    <li><a href="<?= base_url ?>cliente/index" class="waves-effect">Busqueda de cliente</a></li>
-    <li><a href="<?= base_url ?>compra/usuario" class="waves-effect">Mis compras</a></li>
-    <li><a href="<?= base_url ?>servicios/usuario" class="waves-effect">Modificaciones</a></li>
-    <?php if ($_SESSION['identity']['Jerarquia'] == 'Administrador') : ?>
-        <li><a class="subheader">Administración</a></li>
-        <li><a href="<?= base_url ?>compra/index" class="waves-effect">Compras</a></li>
-        <li><a href="<?= base_url ?>verificar/index" class="waves-effect">Verificar servicios</a></li>
-        <li><a href="<?= base_url ?>reportes/index" class="waves-effect">Reportes</a></li>
-        <li><a href="<?= base_url ?>corte/index" class="waves-effect">Corte</a></li>
-        <li><a href="" class="waves-effect">Utilidades</a></li>
-        <li><a href="" class="waves-effect">Usuarios</a></li>
-    <?php endif ?>
-    <li>
-        <div class="divider"></div>
-    </li>
-    <li><a href="<?= base_url ?>login/logout" class="waves-effect">Cerrar sesión</a></li>
-</ul>
+        </li>
+        <li><a href="<?= base_url ?>login/logout" class="waves-effect">Cerrar sesión <i class="material-icons">exit_to_app</i></a></li>
+        <li>
+            <div class="divider"></div>
+        </li>
+        <li><a class="subheader">Operaciones</a></li>
+        <li><a href="<?= base_url ?>servicios/index" class="waves-effect">Venta de servicio <i class="material-icons">point_of_sale</i></a></li>
+        <li><a href="<?= base_url ?>cliente/index" class="waves-effect">Busqueda de cliente <i class="material-icons">person_search</i></a></li>
+        <li><a href="<?= base_url ?>compra/usuario" class="waves-effect">Mis compras <i class="material-icons">shopping_cart</i></a></li>
+        <li><a href="<?= base_url ?>servicios/usuarios" class="waves-effect">Modificaciones <i class="material-icons">settings</i></a></li>
+        <?php if ($_SESSION['identity']['Jerarquia'] == 'Administrador') : ?>
+            <li>
+                <div class="divider"></div>
+            </li>
+            <li><a class="subheader">Administración</a></li>
+            <li><a href="<?= base_url ?>compra/index" class="waves-effect">Compras</a></li>
+            <li><a href="<?= base_url ?>reportes/index" class="waves-effect">Reportes <i class="material-icons">plagiarism</i></a></li>
+            <li><a href="<?= base_url ?>corte/index" class="waves-effect">Corte <i class="material-icons">content_cut</i></a></li>
+            <!-- Herramientas -->
+            <li class="no-padding">
+                <ul class="collapsible collapsible-accordion">
+                    <li>
+                        <a class="collapsible-header">Herramientas <i class="material-icons">construction</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="<?= base_url ?>verificar/index" class="waves-effect">Verificar servicios <i class="material-icons">done_all</i></a></li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+            </li>
+            <!-- Nemi -->
+            <li class="no-padding">
+                <ul class="collapsible collapsible-accordion">
+                    <li>
+                        <a class="collapsible-header">NEMI <i class="material-icons">call</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="<?= base_url ?>utilidad/index">Agregar sims <i class="material-icons">add</i></a></li>
+                                <li><a href="<?= base_url ?>utilidad/index">Reporte sims <i class="material-icons">format_list_numbered</i></a></li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+            </li>
+            <!-- STEL Order -->
+            <li class="no-padding">
+                <ul class="collapsible collapsible-accordion">
+                    <li>
+                        <a class="collapsible-header">STEL Order <i class="material-icons">wysiwyg</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="#">Compras <i class="material-icons">shopping_cart</i></a></li>
+                                <li><a href="#">Ventas <i class="material-icons">point_of_sale</i></a></li>
+                                <li><a href="#">Dinero <i class="material-icons">payments</i></a></li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+            </li>
+        <?php endif ?>
+    </ul>
+</div>
